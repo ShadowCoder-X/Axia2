965b898f13cc0228898df99ba57df958ea29fbbe
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-12 08:51:19 +0100

    Tried another ternary method of if else statement

--- Profile/src/Greeting.jsx
+++ Profile/src/Greeting.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 
-const now = new Date();
-const currentHour = now.getHours();
+// const now = new Date();
+// const currentHour = now.getHours();
 
-let Greet;
+// let Greet;
 
-if (currentHour < 8) {
-  Greet = "Good Morning, My princess.";
-} else if (currentHour < 14) {
-  Greet = "Good Afternoon, My Supreme Leader";
-} else if (currentHour < 17) {
-  Greet = "Good Evening, My Sweet Love";
-} else {
-  Greet = "Good Night";
-}
+// if (currentHour < 8) {
+//   Greet = "Good Morning, My princess.";
+// } else if (currentHour < 14) {
+//   Greet = "Good Afternoon, My Supreme Leader";
+// } else if (currentHour < 17) {
+//   Greet = "Good Evening, My Sweet Love";
+// } else {
+//   Greet = "Good Night";
+// }
+
+let time = 5;
+const Greet =
+  time > 8
+    ? "Good Morning, My princess."
+    : time > 5
+      ? "Good Afternoon, My Supreme Leader."
+      : time > 2
+        ? "Good Evening, My Sweet Love."
+        : "HELLO";
 
 function Greeting() {
   return (
