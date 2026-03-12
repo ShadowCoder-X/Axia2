bc138a670d35c0ec0ebdb22f3628f8e432a4942c
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-12 08:11:13 +0100

    Added Card and Button components to App.jsx

--- Profile/src/App.jsx
+++ Profile/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./App.css";
 import Card from "./Card";
 import Button from "./Button";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Card />
+      <Button />
+    </div>
+  );
 }
 
 export default App;
--- Profile/src/Card.jsx
+++ Profile/src/Card.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import pic from "./assets/images/pic12.jpeg";
 function Card() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="profile">
+        <div className="card">
+          <div className="img">
+            <img src="/assets/images/pic12.jpeg" alt="" />
+          </div>
+
+          <h1 className="heading">Hello World!</h1>
+
+          <h3 className="name"></h3>
+
+          <h3 className="role"></h3>
+
+          <h3 className="bio"></h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
