30b141d2c8c04323deb592cd64f737dae9708c29
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-13 16:39:01 +0100

    Created profile2 to practice useState

---
+++ Profile2/src/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div>
+      <button className="btn">Press this button to move forward</button>
+    </div>
+  );
+}
+
+export default Button;
---
+++ Profile2/src/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Card(props) {
+  // const {name, role, hobby, pic, bio } = props
+  return (
+    <div>
+      <div className="profile">
+        <div className="card">
+          <div className="img">
+            <img src={props.pic} alt={props.name} />
+          </div>
+
+          <h3 className="name">Name: {props.name}</h3>
+
+          <h3 className="role">Role: {props.role}</h3>
+
+          <h3 className="hobby">Hobby: {props.hobby}</h3>
+
+          <h3 className="bio">Bio: {props.bio}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
