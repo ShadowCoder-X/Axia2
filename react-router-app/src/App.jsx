a5db7a1e44cd49c0e688705ff166f5357dce7ee6
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-16 00:06:48 +0100

    Setup React Router project with Tailwind and project folders

---
+++ react-router-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./component/Header";
+import Navigation from "./component/Navigation";
+import Footer from "./component/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <Navigation />
+      <h1 class="text-3xl font-bold underline text-green-700 text-center">
+        Hello world!
+      </h1>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ react-router-app/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2 className="">This is about us</h2>
+    </div>
+  );
+}
+
+export default About;
