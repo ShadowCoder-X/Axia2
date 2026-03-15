ede3ef031aa66a9b4b73222401bf70f80c3de21e
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-15 19:09:19 +0100

    Updating counter with Toggle

--- Profile2/src/Counter.jsx
+++ Profile2/src/Counter.jsx
@@ -7,27 +7,38 @@ const Counter = () => {
 
   function handleIncrement() {
     console.log("hello");
-    setCount("heLLOO");
+    // setCount("heLLOO");
+    setCount(count + 1);
   }
 
   function handleDecrement() {
     console.log("Decrease!!!");
+    setCount(count - 1);
   }
 
   return (
-    <div>
-      <h2>Counter</h2>
+    <>
+      <div className="counter">
+        <h2>Counter</h2>
 
-      <h3>Count: {count}</h3>
+        <h3>Count: {count}</h3>
 
-      <button onClick={handleIncrement}>Increase</button>
+        <button onClick={handleIncrement}>Increase</button>
 
-      {/* <button onClick={() => setCount((prev) => prev + 1)}>Increase</button> */}
+        {/* <button onClick={() => setCount((prev) => prev + 1)}>Increase</button> */}
 
-      <button>Reset</button>
+        <button>Reset</button>
 
-      <button onClick={handleDecrement}>Decrease</button>
-    </div>
+        <button onClick={handleDecrement}>Decrease</button>
+      </div>
+
+      {/*light switching */}
+      <div>
+        <h3>Light Switch</h3>
+        <p>This light bulb is OFF</p>
+        <button>Press to Switch ON</button>
+      </div>
+    </>
   );
 };
 
