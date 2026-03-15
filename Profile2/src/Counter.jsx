ff6f605a17f82f1bfab05630d4f16b62a48fbd08
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-15 18:36:03 +0100

    Displaying setCount on the brower

--- Profile2/src/Counter.jsx
+++ Profile2/src/Counter.jsx
@@ -2,18 +2,31 @@ import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  console.log(count);
-  console.log(setCount);
+  //   console.log(count);
+  //   console.log(setCount);
+
+  function handleIncrement() {
+    console.log("hello");
+    setCount("heLLOO");
+  }
+
+  function handleDecrement() {
+    console.log("Decrease!!!");
+  }
+
   return (
     <div>
       <h2>Counter</h2>
 
       <h3>Count: {count}</h3>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
+
+      <button onClick={handleIncrement}>Increase</button>
+
+      {/* <button onClick={() => setCount((prev) => prev + 1)}>Increase</button> */}
 
       <button>Reset</button>
 
-      <button>Decrease</button>
+      <button onClick={handleDecrement}>Decrease</button>
     </div>
   );
 };
