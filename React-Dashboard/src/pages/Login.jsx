e63725718f028860b9549520feb28627812d581d
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-20 13:19:22 +0100

    Added style to SIgnUp and Login

--- React-Dashboard/src/pages/Login.jsx
+++ React-Dashboard/src/pages/Login.jsx
@@ -2,56 +2,60 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
-      <form action="" className=" ">
-        <h2>Login</h2>
+    <div className="flex justify-center  w-[100%] h-110 p-6  ">
+      <form action="" className="p-10 text-center bg-gray-200 border-double ">
+        <h2 className="text-3xl font-bold text-black mb-4 ">Login</h2>
 
-        <div>
-          <label for="email">Email: </label>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            value=""
-            //  onChange={handleChange}
-            className=" border 2"
-          />
-        </div>
-
-        <div>
-          <label for="password">Password: </label>
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value=""
-            // onChange={handleChange}
-            className=" border-2"
-          />
-        </div>
-
-        <div>
-          <label>
+        <div className="text-left">
+          <div className="mb-4">
+            <label for="email">Email: </label>
             <input
-              type="checkbox"
-              name="remember"
-              //checked={form.remember}
+              type="text"
+              name="email"
+              placeholder="Email"
+              value=""
               //  onChange={handleChange}
-              className=""
+              className=" border 2"
             />
-            {""}
-            Remember Me
-          </label>
+          </div>
 
-          <a href="#">Forget Password</a>
+          <div className="mb-4">
+            <label for="password">Password: </label>
+
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value=""
+              // onChange={handleChange}
+              className=" border-2"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label>
+              <input
+                type="checkbox"
+                name="remember"
+                //checked={form.remember}
+                //  onChange={handleChange}
+                className=""
+              />
+              {""}
+              Remember Me
+            </label>
+
+            <a href="#" className="mb-3">
+              Forget Password
+            </a>
+          </div>
+
+          <button className="mb-3">Login</button>
+
+          <p className="mb-3">Don't have an account</p>
+
+          <a href="/signup"> Sign Up</a>
         </div>
-
-        <button className="">Login</button>
-
-        <p className="">Don't have an account</p>
-
-        <a href="/signup"> Sign Up</a>
       </form>
     </div>
   );
--- React-Dashboard/src/pages/SignUp.jsx
+++ React-Dashboard/src/pages/SignUp.jsx
@@ -4,60 +4,76 @@ import password_icon from "../assets/password.png";
 
 function SignUp() {
   return (
-    <div>
-      <form action="" className="">
-        <h2 className="">Sign Up</h2>
+    <div
+      className="flex justify-center size- 40 max-auto
+    h-155 p-6 "
+    >
+      <form
+        action=""
+        className="p-10 text-center bg-gray-200 border-double w-120"
+      >
+        <h2 className="text-3xl font-bold text-black mb-4 ">Sign Up</h2>
 
-        <div>
-          <img src={user_icon} alt="user-icon" className="w-6 h-6" />
-          <label for="full-name">Full Name: </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Full Name"
-            //  value={}
-            //  onChange={}
-            className="border-2"
-          />
-        </div>
+        <div className="text-left">
+          <div>
+            <img src={user_icon} alt="user-icon" className="w-6 h-6" />
+            <label for="full-name">Full Name: </label>
+            <br />
+            <input
+              type="text"
+              name="name"
+              placeholder="Full Name"
+              //  value={}
+              //  onChange={}
+              className="border-2"
+            />
+          </div>
 
-        <div>
-          <img src={email_icon} alt="email-icon" className="w-6 h-6" />
-          <label for="email">Eamil: </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            //  value={}
-            //  onChange={}
-            className="border-2"
-          />
-        </div>
+          <div>
+            <img src={email_icon} alt="email-icon" className="w-6 h-6" />
+            <label for="email">Eamil: </label>
+            <br />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              //  value={}
+              //  onChange={}
+              className="border-2"
+            />
+          </div>
 
-        <div>
-          <img src={password_icon} alt="password-icon" className="w-6 h-6" />
-          <label for="password">Password: </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            //  value={}
-            //  onChange={}
-            className=" border-2"
-          />
-        </div>
+          <div>
+            <img src={password_icon} alt="password-icon" className="w-6 h-6" />
+            <label for="password">Password: </label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              //  value={}
+              //  onChange={}
+              className=" border-2"
+            />
+          </div>
 
-        <div>
-          <img src={password_icon} alt="password-icon" className="w-6 h-6" />
-          <label for="confirm-password">Confirm Password: </label>
-          <input
-            type="password"
-            name="confirmpassword"
-            placeholder="Confirm Password"
-            //  value={}
-            //  onChange={}
-            className="border-2"
-          />
+          <div>
+            <img src={password_icon} alt="password-icon" className="w-6 h-6" />
+            <label for="confirm-password">Confirm Password: </label>
+            <br />
+            <input
+              type="password"
+              name="confirmpassword"
+              placeholder="Confirm Password"
+              //  value={}
+              //  onChange={}
+              className="border-2"
+            />
+          </div>
+
+          <button className="">Sign Up</button>
+
+          <p className="Already have an account? "></p>
         </div>
       </form>
     </div>
