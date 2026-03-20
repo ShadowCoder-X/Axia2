c1d50afa08a1c1ab4ea0bf8273e86fe0ef5382e1
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-20 19:35:34 +0100

    Implementing design structure in Dashboard.jsx

--- React-Dashboard/src/pages/Dashboard.jsx
+++ React-Dashboard/src/pages/Dashboard.jsx
@@ -4,8 +4,23 @@ function Dashboard() {
   return (
     <div>
       <div>
-        <h2>Welcome To Your Dashboard</h2>
+        <h1>Dashboard</h1>
       </div>
+
+      <nav>
+        <a href="/">Home</a>
+        <a href="/feature">Features</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/login">Login</a>
+        <a href="/signup">SignUp</a>
+      </nav>
+
+      <header>
+        <h2>Welcome to Dashboard Overview</h2>
+      </header>
+
+      <article> This is where the whole will be</article>
     </div>
   );
 }
