7408ab9c8a842509b4103d4286a2a9386c9a0887
omoladeajala <shadowcoderx.02@gmail.com>
2026-03-22 15:55:34 +0100

    chore: install TailwindCSS and React Router

---
+++ DASHBOARD../src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center  text-4xl p-4 text-green-400">OMOLADE</h1>
+    </div>
+  );
+}
+
+export default App;
